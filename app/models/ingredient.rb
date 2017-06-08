17ae737dbad062e38dcde361ae5b5df4ad8e0ee2
luisrog <luisrdzovejerogomez@gmail.com>
2017-06-08 13:14:41 +0200

    Fixed model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  has_many :doses, dependent: :destroy
+  has_many :cocktails, through: :doses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+c = Cocktail.create(name: "Mojito")
+i = Ingredient.create(name: "Lima")
+Dose.create(description: "a lot", cocktail: c, ingredient: i)
